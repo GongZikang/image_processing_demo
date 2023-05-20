b5da37e99de412ed661a174e32ede27bd3c871ab
gongzikang <gongzikang@outlook.com>
2023-05-20 17:14:59 +0800

    添加初始提醒图片 重构ui布局，可拉伸窗口适配各元素大小

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ class ImageProcessingWindow(QMainWindow):
         self.setWindowTitle('Image Processing')
         # 从文件中加载UI定义
         self.ui = uic.loadUi("main.ui")
-        self.ui.pushButton_12.clicked.connect(self.open_image)
+        # 加载初始提醒图片
+        pix = QPixmap(r'init.jpg')
+        self.ui.image_label.setPixmap(pix.scaled(self.ui.image_label.size(), Qt.KeepAspectRatio))
+
+        self.ui.b_open_image.clicked.connect(self.open_image)
+        self.ui.b_save_image.clicked.connect(self.save_image)
 
     def open_image(self):
         file_dialog = QFileDialog()
@@ -20,13 +25,20 @@ class ImageProcessingWindow(QMainWindow):
             image = cv2.imread(file_path)
             image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             height, width, channel = image_rgb.shape
+            print(height+width+channel)
             qimage = QImage(image_rgb.data, width, height, width * channel, QImage.Format_RGB888)
             pixmap = QPixmap.fromImage(qimage)
-            print(self.ui)
-            print(self.ui.label_5)
-            self.ui.label_5.setPixmap(pixmap.scaled(self.ui.label_5.size(), Qt.KeepAspectRatio))
+
+            self.ui.image_label.setPixmap(pixmap.scaled(self.ui.image_label.size(), Qt.KeepAspectRatio))
 
             self.image = image
+    def save_image(self):
+        if self.image is not None:
+            file_dialog = QFileDialog()
+            file_path = file_dialog.getSaveFileName(self, 'Save Image')[0]
+            print(file_path)
+            if file_path:
+                cv2.imwrite(file_path, self.image)
 
 # class ImageProcessingApp(QWidget):
 #     def __init__(self):
